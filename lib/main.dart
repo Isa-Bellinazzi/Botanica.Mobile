f51ff24015fa5638cb2a505490014d5f06e6743c
Isabella Bellinazzi <ibellinazzi@magnasistemas.com.br>
2023-05-09 12:38:26 -0300

    Primeiro Commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MagnaBotanics());
+}
+
+class MagnaBotanics extends StatelessWidget {
+  const MagnaBotanics({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+}
+}
